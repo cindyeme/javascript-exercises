9c11c17bbc5a216c89d894dd4a367bbc0c738e67
cindyeme <emereninicynthia@gmail.com>
2023-08-22 14:54:45 +0100

    solved 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr) {
+  /*
+  arr.filter((val)=>!valsToRemove.includes(val));
+  (arr, ...valsToRemove) {
+    return arr.filter(elem => !valsToRemove.includes(elem));
+  }
+  */
+  
+  const valuesToRemove = Array.from(arguments).slice(1);
+  const newArr = [];
+  for (const item of arr) {
+    if (!valuesToRemove.includes(item)) {
+      newArr.push(item);
+    }
+  }
 
+  return newArr;
 };
 
 // Do not edit below this line
