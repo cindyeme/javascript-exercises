d07e9ddd10287eb6ff8d5f27b728157f49b26ee8
cindyeme <emereninicynthia@gmail.com>
2023-08-22 16:41:02 +0100

    solved 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (
+    typeof num2 !== "number" ||
+    typeof num1 !== "number" ||
+    num1 < 0 ||
+    num2 < 0
+  ) {
+    return "ERROR";
+  }
+  let max = Math.max(num1, num2);
+  let min = Math.min(num1, num2);
+  let sumBetween = 0;
 
+  for (let i = min; i <= max; i++) {
+    sumBetween += i;
+  }
+  return sumBetween;
 };
 
 // Do not edit below this line
