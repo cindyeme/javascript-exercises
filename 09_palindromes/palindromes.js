b1691f6c1ec2ee00b5ffba59f5a4041b4e83dff1
cindyeme <emereninicynthia@gmail.com>
2023-09-11 13:31:59 +0100

    solved 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const newStr = str.replace(/[\W_]/g, '').toLowerCase();
+  const modWord = newStr.split("").reverse().join("");
 
+  if (modWord === newStr) {
+    return true;
+  }
+
+  return false;
 };
 
 // Do not edit below this line
